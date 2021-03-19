ff31c371c5ce9bf2a1cb1fe388a363844d2bc12a
ischmidls <izak.schmidlkofer@gmail.com>
2021-03-19 07:47:51 -0700

    Commit

---
+++ game.js
@@ -0,0 +1,30 @@
+// game.js
+var canvas;
+var canvasContext;
+let ballX = 50;
+let ballY = 50;
+
+window.onload = function() {
+	alert("game.html loaded");
+	canvas = document.getElementById("gameCanvas");
+	canvasContext = canvas.getContext("2d");
+	
+	var framesPerSecond = 30;
+	setInterval(function() {
+		moveEverything();
+		drawEverything();
+	}, 1000/framesPerSecond);
+}
+
+function moveEverything() {
+	ballX += 5;
+}
+
+function drawEverything() {
+	canvasContext.fillStyle = 'black';
+	canvasContext.fillRect(0,0,canvas.width,canvas.height);
+	canvasContext.fillStyle = 'white';
+	canvasContext.fillRect(ballX, ballY, 20, 20);
+	canvasContext.fillStyle = 'white';
+	canvasContext.fillRect(0, ballY, 20, 100);
+}
